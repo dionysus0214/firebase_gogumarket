1e3481d79a9ea9e91f0797c36af051eb620eab73
dionysus0214 <hiba0214@naver.com>
2021-11-25 23:56:50 +0900

    functions로 알림기능 만들기

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,17 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.hello = functions.region('asia-northeast3').https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("안녕하세요");
+});
+
+exports.createAlert = functions.region('asia-northeast3')
+.firestore.document('chatroom/{docid}').onCreate((snapshot, context)=>{
+  var product = snapshot.data().product;
+  var who= snapshot.data().who;
+  
+  db.collection('user').doc(who[0]).update({ alert: '어떤놈이 채팅검' })
+  db.collection('user').doc(who[1]).update({ alert : '어떤놈이 채팅검' })
+}); 
